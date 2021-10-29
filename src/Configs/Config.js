c25edeacd99b4be12b9101f4a41bde0cfc38f560
Luis Angel <luisparr14@gmail.com>
2021-10-29 12:30:47 -0500

    update urls and remoce analytics firebase

--- src/Configs/Config.js
+++ src/Configs/Config.js
@@ -2,26 +2,28 @@
 import axios from 'axios';
 
 let urls = [
-  'https://restauranked-api.herokuapp.com/api',
-  'http://192.168.56.1:3001/api',
-  'http://192.168.122.1:3001/api',
-  'http://192.168.1.13:3001/api',
-  'http://192.168.122.1:3001/api',
+  'https://restauranked.herokuapp.com',
+  'http://192.168.56.1:3001',
+  'http://192.168.122.1:3001',
+  'http://192.168.1.13:3001',
+  'http://192.168.122.1:3001',
 ];
 
 var ruta = '';
 
 export const url = () => {
   for (let i = 0; i < urls.length; i++) {
-    const requestUrl = urls[i].replace('/api', '');
+    const requestUrl = urls[i];
     axios(requestUrl)
       .then(res => {
         if (res !== undefined) {
           ruta = res.request.responseURL;
         }
       })
-      .catch(() => { });
-    }
+      .catch(() => {
+        console.log('error');
+       });
+  }
   return ruta + 'api';
 };
 
